bc5ab1b8234b5308abe2e2470a9ef89376278869
OpaaL1 <naufali429@gmail.com>
2026-01-28 13:54:52 +0700

    menambahkan Kerangka Tugas

--- src/App.js
+++ src/App.js
@@ -9,23 +9,23 @@ function App() {
 
   return (
     <div className="container-fluid bg-dark text-white vh-100 p-0 overflow-hidden">
-      {/* Header Aplikasi */}
+      /* Header Aplikasi */
       <header className="p-3 border-bottom border-secondary bg-black">
         <h4 className="m-0 text-success fw-bold">ATC COLLISION PREDICTION SYSTEM</h4>
       </header>
 
       <div className="row g-0 h-100">
-        {/* KOLOM KIRI: Kontrol (Fitur Wajib 1) */}
+        /* KOLOM KIRI: Kontrol */
         <div className="col-md-3 border-end border-secondary p-4 bg-opacity-10">
           <ControlPanel points={points} setPoints={setPoints} />
         </div>
 
-        {/* KOLOM TENGAH: Visualisasi Radar (Fitur Wajib 2 & 3) */}
+        /* KOLOM TENGAH: Visualisasi Rada */
         <div className="col-md-6 d-flex align-items-center justify-content-center bg-black position-relative">
           <RadarCanvas points={points} />
         </div>
 
-        {/* KOLOM KANAN: Analisis Empiris */}
+        /* KOLOM KANAN: Analisis Empiris */
         <div className="col-md-3 border-start border-secondary p-4">
           <h5 className="text-info">Algorithm Analysis</h5>
           <hr className="bg-secondary" />
